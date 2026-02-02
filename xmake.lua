bdb6b9ae50e73b1ba0a11a42d3d80402585424bd
zerobut1 <1181948754@qq.com>
2026-02-02 12:45:57 +0800

    update LC, fix glfw build

--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,23 @@ set_defaultarchs("x64")
 set_languages("cxx20")
 set_encodings("utf-8")
 
-add_requires("glfw", {configs = {shared = true}})
 add_requires("tinyexr")
 add_requires("assimp")
 
 includes("ext/LuisaCompute")
-includes("config.lua")
 includes("src")
+
+lc_options = {
+    lc_cuda_backend = true,
+    lc_dx_backend = true,
+    lc_vk_backend = false,
+    lc_metal_backend = false,
+    lc_enable_gui = true,
+    lc_enable_osl = false,
+    lc_enable_dsl = true,
+    lc_enable_tests = false,
+    lc_enable_mimalloc = true,
+    lc_enable_simd = true,
+    lc_enable_unity_build = true,
+    lc_dx_cuda_interop = true,
+}

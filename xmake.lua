b20aeca58dd0143259cd55c07ba510fdd101322b
zerobut1 <1181948754@qq.com>
2026-02-25 19:49:23 +0800

    update LC

--- xmake.lua
+++ xmake.lua
@@ -6,39 +6,26 @@ set_encodings("utf-8")
 add_requires("tinyexr")
 add_requires("assimp")
 
-local lc_options_win = {
-    lc_cuda_backend = true,
-    lc_dx_backend = true,
-    lc_vk_backend = false,
-    lc_metal_backend = false,
-    lc_enable_gui = true,
-    lc_enable_osl = false,
-    lc_enable_dsl = true,
-    lc_enable_tests = false,
-    lc_enable_mimalloc = true,
-    lc_enable_simd = true,
-    lc_enable_unity_build = true,
-    lc_dx_cuda_interop = true,
-}
-
-local lc_options_macos = {
-    lc_cuda_backend = false,
-    lc_dx_backend = false,
-    lc_vk_backend = false,
-    lc_metal_backend = true,
-    lc_enable_gui = true,
-    lc_enable_osl = false,
-    lc_enable_dsl = true,
-    lc_enable_tests = false,
-    lc_enable_mimalloc = true,
-    lc_enable_simd = true,
-    lc_enable_unity_build = true,
-}
-
-if is_plat("windows") then
-    lc_options = lc_options_win
-elseif is_plat("macosx") then
-    lc_options = lc_options_macos
+if is_host("windows") then
+    lc_options = {
+        lc_cuda_backend = true,
+        lc_dx_backend = true,
+        lc_vk_backend = false,
+        lc_metal_backend = false,
+        lc_enable_gui = true,
+        lc_enable_tests = false,
+        lc_enable_osl = false,
+    }
+elseif is_host("macosx") then
+    lc_options = {
+        lc_cuda_backend = false,
+        lc_dx_backend = false,
+        lc_vk_backend = false,
+        lc_metal_backend = true,
+        lc_enable_gui = true,
+        lc_enable_tests = false,
+        lc_enable_osl = false,
+    }
 end
 
 includes("ext/LuisaCompute")

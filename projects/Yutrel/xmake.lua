21e0d2f2d81d3029584fccd135299518fbf397a7
zerobut1 <1181948754@qq.com>
2026-07-16 16:15:04 +0800

    YutrelCore静态库

--- projects/Yutrel/xmake.lua
+++ projects/Yutrel/xmake.lua
@@ -1,15 +1,20 @@
+target("YutrelCore")
+    set_kind("static")
+    add_files("src/**.cpp")
+    remove_files("src/main.cpp")
+    add_headerfiles("src/**.h")
+    add_includedirs("src", {public = true})
+
+    add_deps("lc-dsl", "lc-gui", "stb-image")
+    add_packages("tinyexr", "assimp", {public = true})
+target_end()
+
 target("Yutrel")
     set_kind("binary")
     set_rundir("$(projectdir)/projects/Yutrel")
 
-    add_files("src/**.cpp")
-    add_headerfiles("src/**.h")
-
-    add_includedirs("src")
-
-    add_deps("lc-dsl", "lc-gui", "stb-image")
-    add_packages("tinyexr")
-    add_packages("assimp")
+    add_files("src/main.cpp")
+    add_deps("YutrelCore")
 
 target_end()
 
